b04130701f85a72fa24a7bf76c13f222105d1b19
asankhaperera <asankha.perera@gmail.com>
2020-03-27 15:17:35 +0530

    init

---
+++ AlexaTest.js
@@ -0,0 +1,173 @@
+const Alexa = require("ask-sdk");
+const https = require("https");
+const invocationName = "test sigma";
+
+// 1. Intent Handlers =============================================
+
+const AMAZON_FallbackIntent_Handler =  {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.FallbackIntent' ;
+    },
+    handle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        let say = 'Sorry I didnt catch what you said';
+        return responseBuilder
+            .speak(say)
+            .reprompt(say)
+            .getResponse();
+    },
+};
+
+const AMAZON_CancelIntent_Handler =  {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.CancelIntent' ;
+    },
+    handle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        let say = 'Okay, talk to you later! ';
+        return responseBuilder
+            .speak(say)
+            .withShouldEndSession(true)
+            .getResponse();
+    },
+};
+
+const AMAZON_HelpIntent_Handler =  {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.HelpIntent' ;
+    },
+    handle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        let say = 'You asked for help. '; 
+        return responseBuilder
+            .speak(say)
+            .reprompt('try again, ' + say)
+            .getResponse();
+    },
+};
+
+const AMAZON_StopIntent_Handler =  {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.StopIntent' ;
+    },
+    handle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        let say = 'Okay, talk to you later! ';
+        return responseBuilder
+            .speak(say)
+            .withShouldEndSession(true)
+            .getResponse();
+    },
+};
+
+const AMAZON_NavigateHomeIntent_Handler =  {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.NavigateHomeIntent' ;
+    },
+    handle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        let say = 'Hello from AMAZON.NavigateHomeIntent. ';
+        return responseBuilder
+            .speak(say)
+            .reprompt('try again, ' + say)
+            .getResponse();
+    },
+};
+
+const greetMe_Handler =  {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' && request.intent.name === 'greetMe' ;
+    },
+    handle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        let say = 'Hello from greetMe. ';
+        return responseBuilder
+            .speak(say)
+            .reprompt('try again, ' + say)
+            .getResponse();
+    },
+};
+
+const LaunchRequest_Handler =  {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const responseBuilder = handlerInput.responseBuilder;
+        let say = 'hello' + ' and welcome to ' + invocationName + ' ! Say help to hear some options.';
+
+        return responseBuilder
+            .speak(say)
+            .reprompt('try again, ' + say)
+            .getResponse();
+    },
+};
+
+const SessionEndedHandler =  {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+const ErrorHandler =  {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const request = handlerInput.requestEnvelope.request;
+
+        console.log(`Error handled: ${error.message}`);
+        // console.log(`Original Request was: ${JSON.stringify(request, null, 2)}`);
+
+        return handlerInput.responseBuilder
+            .speak(`Sorry, your skill got this error.  ${error.message} `)
+            .reprompt(`Sorry, your skill got this error.  ${error.message} `)
+            .getResponse();
+    }
+};
+
+// 4. Exports handler function and setup ===================================================
+const skillBuilder = Alexa.SkillBuilders.standard();
+let myGeneratedHandler = skillBuilder
+    .addRequestHandlers(
+        AMAZON_FallbackIntent_Handler, 
+        AMAZON_CancelIntent_Handler, 
+        AMAZON_HelpIntent_Handler, 
+        AMAZON_StopIntent_Handler, 
+        AMAZON_NavigateHomeIntent_Handler, 
+        greetMe_Handler, 
+        LaunchRequest_Handler, 
+        SessionEndedHandler
+    )
+    .addErrorHandlers(ErrorHandler)
+    .addRequestInterceptors(InitMemoryAttributesInterceptor)
+    .addRequestInterceptors(RequestHistoryInterceptor)
+    .lambda();
+
+exports.handler = async (event) => {
+    return await myGeneratedHandler.handle.handle();
+};
